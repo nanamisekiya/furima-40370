4c5b7f98c13e39c377f55177691e32223c84f1b7
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-11 21:38:16 +0900

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_07_071303) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_11_122503) do
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.text "product_description", null: false
+    t.integer "product_category_id", null: false
+    t.integer "product_status_id", null: false
+    t.integer "shipping_fee_id", null: false
+    t.integer "shippind_area_id", null: false
+    t.integer "delivery_time_id", null: false
+    t.integer "product_price", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
