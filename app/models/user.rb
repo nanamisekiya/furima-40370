7f05124ae56756218fa9cedf845dfab0a125703b
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-11 22:55:44 +0900

    エラー・バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
+
 end
