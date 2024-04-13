ef8f291df8206d6daadce980704f56fff1050bbe
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-14 00:11:30 +0900

    delivery_timeの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to :product_status
   belongs_to :shipping_fee
   belongs_to :shipping_area_id
+  belongs_to :delivery_time_id
 
   validates :product_name, presence: true
   validates :product_description, presence: true
@@ -12,7 +13,7 @@ class Item < ApplicationRecord
   validates :product_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_area_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_time_id, presence: true
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :product_price, presence: true
 
 end
