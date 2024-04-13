7c665d0277d22296d88aac7ebc18c13017abd86f
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-13 22:05:54 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :product_description, :product_category_id, :product_status_id, :shipping_fee_id, :shipping_area_id, :delivery_time_id, :product_price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :product_name, presence: true
+  validates :product_description, presence: true
+  validates :product_category_id, presence: true
+  validates :product_status_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :delivery_time_id, presence: true
+  validates :product_price, presence: true
 end
