008b6882ef3b0af152af6262f4fe79a80a7f9b99
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-13 23:58:06 +0900

    shipping_feeの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :product_category
   belongs_to :product_status
+  belongs_to :shipping_fee
 
   validates :product_name, presence: true
   validates :product_description, presence: true
   validates :product_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :product_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_fee_id, presence: true
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_area_id, presence: true
   validates :delivery_time_id, presence: true
   validates :product_price, presence: true
