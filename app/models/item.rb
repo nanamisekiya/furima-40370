8ada74288c3d9668191b7aac8922ed9d8aed904c
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-13 23:53:07 +0900

    product_statusの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :product_category
+  belongs_to :product_status
 
   validates :product_name, presence: true
   validates :product_description, presence: true
-  validates :product_category_id, presence: true
-  validates :product_status_id, presence: true
+  validates :product_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :product_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id, presence: true
   validates :shipping_area_id, presence: true
   validates :delivery_time_id, presence: true
   validates :product_price, presence: true
 
-  validates :product_category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
