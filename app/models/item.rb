38dd41c5392f9e5751a42b64b982c6e1c6b4b6f2
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-13 23:44:06 +0900

    product_categoryの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :product_category
 
   validates :product_name, presence: true
   validates :product_description, presence: true
@@ -10,4 +11,6 @@ class Item < ApplicationRecord
   validates :shipping_area_id, presence: true
   validates :delivery_time_id, presence: true
   validates :product_price, presence: true
+
+  validates :product_category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
