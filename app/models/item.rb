421e3e47a8eeb74150cdba3a99029fa118c914dd
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-14 00:05:13 +0900

    shipping_areaの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,14 @@ class Item < ApplicationRecord
   belongs_to :product_category
   belongs_to :product_status
   belongs_to :shipping_fee
+  belongs_to :shipping_area_id
 
   validates :product_name, presence: true
   validates :product_description, presence: true
   validates :product_category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :product_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_area_id, presence: true
+  validates :shipping_area_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_time_id, presence: true
   validates :product_price, presence: true
 
