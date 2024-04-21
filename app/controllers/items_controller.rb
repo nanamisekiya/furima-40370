0aac6ef379310c444a6221b874d054a0e0775f64
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-04-21 20:28:37 +0900

    rubocopの実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user.id
-      redirect_to root_path
-    end
+    return if current_user.id == @item.user.id
+
+    redirect_to root_path
   end
 
   def update
