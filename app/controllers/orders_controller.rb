4a2b15f651bc9c4be12caaa24cf0730aff01a888
nanamisekiya <na511mi1009.3chigai@gmail.com>
2024-05-06 13:47:37 +0900

    商品購入の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,21 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_purchase = OrderPurchase.new(order_purchase_params)
+    if @order_purchase.valid?
+      @order_purchase.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_purchase_params
+    params.permit(:post_code, :shipping_area_id, :city, :block, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
